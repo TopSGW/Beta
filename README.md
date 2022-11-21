93918b78bc20d94bd2d70ab77758f1b26f4eb7d1
TopSGW <web.smartdev22@gmail.com>
2022-11-21 01:24:06 -0800

    first

---
+++ README.md
@@ -0,0 +1,15 @@
+# BlogFeed-v2
+A multi-user blogging application built using MERN (MongoDB, Express, React, Node) stack with cloud-based image and video management service cloudinary.
+# Live Demo:
+https://blogfeedv2.herokuapp.com
+# Tech stack: 
+React, Redux, Node, Express, MongoDB
+
+# Snapshots:
+
+![](https://github.com/arghac14/BlogFeed-v2/blob/master/snapshots/Capture4.JPG)
+![](https://github.com/arghac14/BlogFeed-v2/blob/master/snapshots/Capture2.PNG)
+![](https://github.com/arghac14/BlogFeed-v2/blob/master/snapshots/Capture3.PNG)
+
+## Verion 1.0:
+https://github.com/arghac14/BlogFeed
---
+++ client/precache-manifest.371810725802048c82144bbebda9bfda.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf0e52d4616ff69547aeff94754a082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bce02fdeeb96b2f7de5",
+    "url": "/static/css/2.ddfacf9b.chunk.css"
+  },
+  {
+    "revision": "8b259c5ccf4cae7f66bd",
+    "url": "/static/css/main.42e56194.chunk.css"
+  },
+  {
+    "revision": "1bce02fdeeb96b2f7de5",
+    "url": "/static/js/2.da38b903.chunk.js"
+  },
+  {
+    "revision": "279eeb1279dc341d86a12f6058999a83",
+    "url": "/static/js/2.da38b903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b259c5ccf4cae7f66bd",
+    "url": "/static/js/main.481c33e9.chunk.js"
+  },
+  {
+    "revision": "a486c5a1aabfb7dd794f",
+    "url": "/static/js/runtime-main.6094c464.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4e0e96d585eb4d4859b53270725d0d4c",
+    "url": "/static/media/logo1.4e0e96d5.PNG"
+  },
+  {
+    "revision": "e95057797eacf6f68107c416e69a52a1",
+    "url": "/static/media/logo3.e9505779.PNG"
+  }
+]);
